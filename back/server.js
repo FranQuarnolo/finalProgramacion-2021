7e9067788cd6b45a60d0a273c14e364e3fdb3d38
Ziima <quarnolofrancisco@gmail.com>
2021-07-24 14:59:35 -0300

    mongodb users

--- back/server.js
+++ back/server.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/comicstore', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.get('/', (req, res) => {
     res.send('Servidor Listo!');
