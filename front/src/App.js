18c3df3ae1ebcb946ca94817ce9db019043a0bcc
Ziima <quarnolofrancisco@gmail.com>
2021-07-26 09:30:08 -0300

    Login funcionamiento, login y logout

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import { signout } from "./actions/usuarioActions";
 import Carrito from "./pages/Carrito";
 import Home from "./pages/Home";
 import ProductoView from "./pages/ProductoView";
-import Login from './pages/Login';
+import Login from "./pages/Login";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
+  const usuarioLogin = useSelector((state) => state.usuarioLogin);
+  const { userInfo } = usuarioLogin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -26,7 +33,22 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/login">SingIn</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/login">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { signin } from "../actions/usuarioActions";
+import Loading from "../components/Loading";
+import MessageBox from "../components/MessageBox";
 
-export default function Login() {
+export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+
+  const usuarioLogin = useSelector((state) => state.usuarioLogin);
+  const { userInfo, loading, error } = usuarioLogin;
+
+  const dispatch = useDispatch();
+
   const submitHandler = (e) => {
     e.preventDefault();
-    // TODO: sign in action
+    dispatch(signin(email, password));
   };
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Iniciar Sesión</h1>
         </div>
+        {loading && <Loading></Loading>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email</label>
           <input
